9e34a3619d40f75a679a6f44d0293b65fc411077
karina leal <89037437+Kakaleal@users.noreply.github.com>
2021-08-23 11:09:53 -0300

    Add files via upload

---
+++ FaseTres/FçoParte.js
@@ -0,0 +1,9 @@
+//entrada 
+var z = prompt('Quantos anos você tem?');
+//processamento 
+var x = 2021 - 1891;
+
+var y = x - z;
+
+//saida 
+console.log( "Teresópolis tem " + x + "anos. Desses, foram " + y + "antes de mim. Mas nos últimos " + z + "anos ela conta comigo!");
