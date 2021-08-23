5046f2d3dff1abbe831fe41e9fa77f472882b274
karina leal <89037437+Kakaleal@users.noreply.github.com>
2021-08-23 10:37:29 -0300

    Delete Indeciso.js

--- FaseZero/Indeciso.js
+++
@@ -1,7 +0,0 @@
-var sorte1= 12;
-var sorte2= 14;
-var sorte3= 19;
-
-console.log ("Meu número da sorte é "+ sorte1);
-console.log ("Meu número da sorte é "+ sorte2);
-console.log ("Meu número da sorte é "+ sorte3);
