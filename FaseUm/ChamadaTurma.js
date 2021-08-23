69b79c09f6634d01d4a7bf735016f6601712efc3
karina leal <89037437+Kakaleal@users.noreply.github.com>
2021-08-23 10:31:16 -0300

    Add files via upload

---
+++ FaseUm/ChamadaTurma.js
@@ -0,0 +1,81 @@
+
+//O programa “Chamada da sala de aula” irá escrever o (primeiro) nome de
+//todos os seus colegas, em ordem alfabética decrescente. Para ter acesso à
+//lista dos nomes, você pode olhar a listagem da turma no Classroom.
+
+
+var alunos =
+ "Carlos Alberto Rodrigues"
+
+"Rafael Alves De Souza"
+
+"Marcella Alzuguir"
+
+"Marcos Antonio De Moraes Junior"
+
+"Luana Aquino Da Silva"
+
+"Angelita Arruda Sobrinho Zimbrão"
+
+"odrigo Calmon Nogueira Da Gama Marques Fontes"
+
+"Daniel Cesar da Costa"
+
+"Viviane Dantas Soares"
+
+"odrigo De Paula Ribeiro"
+
+"Diego Dias Faria Da Silveira"
+
+"Luciana Evangelista Dos Santos"
+
+"Guilherme Ferreira Gusman"
+
+"Leandro Fita"
+
+"Luciana Gasparini Moreira Wendling"
+
+"Nathan Guimarães"
+
+"Higor Henrique Dos Santos Nascimento"
+
+"Pedro Henrique Pinto Ricardo"
+
+"Pedro Henrique Quaresma Coelho"
+
+"Victor Hugo de Oliveira Martins"
+
+"Gustavo Jardim De Barros"
+
+"Gilnei Lima Dos Santos"
+
+"Andre Luiz Curio Cintra"
+
+"Adrian Menezes Da Silva"
+
+"Samuel Miranda Ferreira"
+
+"Raquel Nascimento Machado"
+
+"Beatriz Neves Nolasco"
+
+"Bruno Oliveira De Paula Salerno Ribeiro"
+
+"Núria Peçanha Barbosa"
+
+"Vanderson Pereira Da Silva"
+
+"Eduardo Pina Brigatti"
+
+"Gustavo Ponte Barbosa"
+
+"Andre Ponte Da Silva"
+
+"Matheus Rodrigues Andrade"
+
+"Carolina Tardin"
+
+"Marcos Vinicius Ferraz Gonçalves"
+
+  
+  console.log(alunos);
